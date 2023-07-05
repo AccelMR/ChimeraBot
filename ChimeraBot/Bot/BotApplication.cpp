a94db70dc446455bb6ddacd56c8370d21ce46d01
AccelMR <accel.mr@gmail.com>
2023-07-05 16:33:38 -0600

    removed comments

--- ChimeraBot/Bot/BotApplication.cpp
+++ ChimeraBot/Bot/BotApplication.cpp
@@ -14,20 +14,6 @@ void BotApp::init(chEngineSDK::String token) {
 
   m_discordBot->on_log(dpp::utility::cout_logger());
 
-  //m_discordBot->on_slashcommand([](const dpp::slashcommand_t& event) {
-  //  if (event.command.get_command_name() == "ping") {
-  //    event.reply("Pong!");
-  //  }
-  //  });
-
-  //m_discordBot->on_ready([this](const dpp::ready_t& event) {
-  //  if (dpp::run_once<struct register_bot_commands>()) {
-  //    m_discordBot->global_command_create(
-  //      dpp::slashcommand("ping", "Ping pong!", m_discordBot->me.id)
-  //    );
-  //  }
-  //  });
-
   m_discordBot->on_guild_create([](const dpp::guild_create_t& guild) {
     BotManager::instance().registerBot(guild.created->id);
   });
