66e92f4006e0bd14d94bffa03cec52e73ac5eae3
AccelMR <accel.mr@gmail.com>
2023-07-05 14:28:47 -0600

    Working for linux and windows
    Visual CMakeSettings.json points to a absolute path, not sure how to fix that.

---
+++ ChimeraBot/Bot/Utilities/chPrerequisitesUtilities.h
@@ -0,0 +1,33 @@
+/************************************************************************/
+/**
+ * @file chPrerequisitesUtilities.h
+ * @author AccelMR <accel.mr@gmail.com>
+ * @date 2021/09/10
+ * @brief Utilities include with the most basic defines needed.
+ *
+ * Utilities include with the most basic defines needed for compilation
+ * This is the file that must be included for use on the external objects.
+ *
+ * @bug No bug known.
+ */
+ /************************************************************************/
+#pragma once
+
+
+/************************************************************************/
+/*
+ * Platform specific stuff
+ */
+ /************************************************************************/
+#include "chPlatformDefines.h"
+
+//Platform Types
+#include "chPlatformTypes.h"
+
+//STD headers and wrapper
+#include "chSTDHeaders.h"
+
+//Forward Declarations for Utilities Project
+#include "chForwardDecUtil.h"
+
+#include "chException.h"
